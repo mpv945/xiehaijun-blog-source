eb58c2a68a9608d1ea4f26fa494a90c7bc1fafeb
xiehaijun <zhenxindefeng@126.com>
2022-11-09 00:11:03 +0800

    编译运行java项目,调整获取环境变量方式

--- java/github-actions/src/main/java/com/ewancle/githubactions/business/BaiduDNSSuBS.java
+++ java/github-actions/src/main/java/com/ewancle/githubactions/business/BaiduDNSSuBS.java
@@ -14,19 +14,26 @@ public class BaiduDNSSuBS {
 
     static {
         SU_CLIENT_CONFIG = new SuClientConfig();
-        SU_CLIENT_CONFIG.setAccessKey(System.getProperty("BAIDU_SU_ACCESS_KEY"));
-        SU_CLIENT_CONFIG.setSecretKey(System.getProperty("BAIDU_SU_SECRET_KEY"));
+        SU_CLIENT_CONFIG.setAccessKey(System.getenv("BAIDU_SU_ACCESS_KEY"));
+        SU_CLIENT_CONFIG.setSecretKey(System.getenv("BAIDU_SU_SECRET_KEY"));
         SU_CLIENT_CONFIG.setEndpoint("api.su.baidu.com");
     }
 
     public void purgeCache(){
         System.out.println("准备开始进入清除阶段");
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(System.getProperty("line.separator"));
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("os.name"));
+        String value = System.getenv("HOME");
+        System.out.println("HOME = "+value);
+        
         // 读取操作系统环境变量
         // Map<String, String> osEnv = System.getenv();
         System.getProperties().entrySet().iterator();
 
         Map<String, Object> params = new HashMap<>();
-        params.put("domain", System.getProperty("BLOG_DOMAIN"));
+        params.put("domain", System.getenv("BLOG_DOMAIN"));
         params.put("purge_everything", 1);
 
         Map<String, Object> result = null;
